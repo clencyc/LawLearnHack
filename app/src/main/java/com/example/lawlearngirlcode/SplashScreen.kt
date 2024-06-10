c0c7601f3fc9e928a249f978864ec1044b5117d0
clencyc <clency2023@gmail.com>
2024-06-10 10:14:27 +0300

    Added splash screen

---
+++ app/src/main/java/com/example/lawlearngirlcode/SplashScreen.kt
@@ -0,0 +1,21 @@
+package com.example.lawlearngirlcode
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
+
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var EnterName: TextInputLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash_screen)
+
+        EnterName = findViewById(R.id.entername)
+        }
+}
