5bb14fff7d9668eab2d822893d71e9bff5d78fb3
clencyc <clency2023@gmail.com>
2024-06-27 22:28:02 +0300

    Added quizzes section

--- app/src/main/java/com/example/lawlearning/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/lawlearning/ui/gallery/GalleryFragment.kt
@@ -13,7 +13,7 @@ class GalleryFragment : Fragment() {
 
     private var _binding: FragmentGalleryBinding? = null
 
-    // This property is only valid between onCreateView and
+     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
--- app/src/main/java/com/example/lawlearning/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/lawlearning/ui/home/HomeFragment.kt
@@ -15,7 +15,7 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
+     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
