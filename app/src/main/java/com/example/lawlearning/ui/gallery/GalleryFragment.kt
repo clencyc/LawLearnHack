46be6339aba273fbaeb035ee9c5185887d9e1f96
clencyc <clency2023@gmail.com>
2024-06-28 11:09:54 +0300

    connected layouts

--- app/src/main/java/com/example/lawlearning/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/lawlearning/ui/gallery/GalleryFragment.kt
@@ -17,24 +17,6 @@ class GalleryFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
-
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
-    }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
